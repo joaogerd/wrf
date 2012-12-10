fd3e41fa3585d4c777bc4e95533f601e339a966b
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-12-11 01:19:21 +0900

    * gen-tags.sh: don't use subshell for find

--- gen-tags.sh
+++ gen-tags.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-ctags-exuberant -e -a --verbose=yes --language-force="Fortran" `find ${1:-"."} -type f -name "*.F"`
+find ${1:-"."} -type f -name "*.F" -print0 | xargs -0 \
+    ctags-exuberant -e -a --verbose=yes \
+    --language-force="Fortran"
